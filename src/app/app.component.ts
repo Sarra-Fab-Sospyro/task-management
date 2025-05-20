c827f417ef411d8480fc0fb94b532a0ac0bb015e
Fabio Sarra <f.sarra@nesea.it>
2025-05-20 16:29:07 +0200

    server created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [/*RouterOutlet*/],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-tracker';
+
+  private _http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this._http.get("/test").subscribe();
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { urlPrefixInterceptor } from './core/interceptors/url-prefix.interceptor';
+import { environmentProvider } from './core/token/environment-token';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(
+    withInterceptors([urlPrefixInterceptor])
+  ),
+    environmentProvider]
 };
